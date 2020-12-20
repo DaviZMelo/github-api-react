6a434210270b3e4986fb982f5fb7944dd2199e01
Davi Melo <extremexgamer32@gmail.com>
2020-12-20 12:34:55 -0300

    chore: add themes colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  background: '#F0F0F5',
+  title: '#3A3A3A',
+  buttons: '#04d361',
+  cards: '#FFFFFF',
+};
+
+export const darkTheme = {
+  background: '#121212',
+  title: '#FFFFFF',
+  buttons: '#FF9800',
+  cards: '#424242',
+};
