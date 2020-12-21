2dfc65db0ff851a50d1847cd4b1aa0626d31e9fc
Davi Melo <extremexgamer32@gmail.com>
2020-12-21 03:53:44 -0300

    fix: match the theme and your types

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,23 @@
 export const lightTheme = {
-  primary: '#FFFFFF',
-  secundary: '#04d361',
+  title: 'light',
 
-  background: '#F0F0F5',
-  title: '#3A3A3A',
+  colors: {
+    primary: '#FFFFFF',
+    secundary: '#04d361',
 
+    background: '#F0F0F5',
+    title: '#3A3A3A',
+  }
 };
 
 export const darkTheme = {
-  primary: '#424242',
-  secundary: '#FF9800',
-  
-  background: '#121212',
-  title: '#FFFFFF',
+  title: 'dark',
 
+  colors: {
+    primary: '#424242',
+    secundary: '#FF9800',
+
+    background: '#121212',
+    title: '#FFFFFF',
+  }
 };
