0e3fff4befca3b98971a0681308f7a7b68caece7
Davi Melo <extremexgamer32@gmail.com>
2020-12-21 02:47:06 -0300

    chore: add theme type

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,17 @@
 export const lightTheme = {
+  primary: '#FFFFFF',
+  secundary: '#04d361',
+
   background: '#F0F0F5',
   title: '#3A3A3A',
-  buttons: '#04d361',
-  cards: '#FFFFFF',
+
 };
 
 export const darkTheme = {
+  primary: '#424242',
+  secundary: '#FF9800',
+  
   background: '#121212',
   title: '#FFFFFF',
-  buttons: '#FF9800',
-  cards: '#424242',
+
 };
